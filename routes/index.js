318067999e17f5c4cc17d400bd0406faa9659e4c
gerwo <mustafa82@yandex.ru>
2021-04-20 10:37:09 +0300

    added crash-test page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const { login, createUser } = require('../controllers/users');
 
 const { createUserValidation, loginValidation } = require('../middlewares/celebrate');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', loginValidation, login);
 router.post('/signup', createUserValidation, createUser);
 
