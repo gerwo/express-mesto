32c9bdf94a48bb0a5c55ab9368599b8f0111f3dd
gerwo <mustafa82@yandex.ru>
2021-04-21 19:47:31 +0300

    bugfix

--- app.js
+++ app.js
@@ -27,23 +27,8 @@ mongoose.connect(MONGODB_URL, {
 
 app.use(cors());
 
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-
-app.options('*', cors());
-
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
 
