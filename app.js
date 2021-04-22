eb38f26e3c727fe38c4c95b673ec5baa1be1c561
gerwo <mustafa82@yandex.ru>
2021-04-22 22:43:52 +0300

    bugfix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Expose-Headers', 'Set-Cookie');
   res.append('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Set-Cookie');
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requ  ested-With, Content-Type, Accept, Set-Cookie');
 
   next();
 });
