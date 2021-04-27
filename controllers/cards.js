36a35e91602ca1fc72ca0a4a627f48d353845ee3
gerwo <mustafa82@yandex.ru>
2021-04-27 13:36:19 +0300

    bugfix

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ const BadRequestError = require('../errors/bad-request-err');
 const ForbiddenError = require('../errors/forbidden-err');
 
 const getCards = (req, res, next) => {
-  Card.find({ limit: 18 })
+  Card.find({})
     .then((cards) => {
       res.send(cards);
     })
